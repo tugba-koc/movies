92ea17078f319b4fac2a76ce3a1076c031452bfc
tugba-koc <tugba.koc@sisal.com>
2023-12-04 18:49:05 +0300

    feat: add deleteReview from a specific movie

--- fe/src/api/axiosConfig.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://9f48-2a02-ff0-3304-50d5-a444-db1a-63bc-2801.ngrok-free.app',
-  headers: { 'ngrok-skip-browser-warning': true },
-});
--- fe/src/components/home/Home.jsx
+++
@@ -1,7 +0,0 @@
-import Hero from '../hero/Hero';
-
-const Home = ({ movies }) => {
-  return <Hero movies={movies} />;
-};
-
-export default Home;
